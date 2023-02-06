22b378be13494c5129c5a7a1525eacd0c7697084
Jadson Leitão <114675109+JadsonTSI@users.noreply.github.com>
2023-02-06 11:16:33 -0300

    Delete kernal.py

--- kernal.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-from ctypes import wintypes
-lpBuffer = wintypes.LPWSTR()
-AppUserModelID = ctypes.windll.shell32.GetCurrentProcessExplicitAppUserModelID
-AppUserModelID(ctypes.cast(ctypes.byref(lpBuffer), wintypes.LPWSTR))
-appid = lpBuffer.value
-ctypes.windll.kernel32.LocalFree(lpBuffer)
-if appid is not None:
-    print(appid)
