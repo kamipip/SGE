187ddfaa083c603c9b9b347eed0622e2d2010f6e
KAMILLA AUGUSTO NUNES <20221148060028@ifrn.local>
2023-02-06 11:57:39 -0300

    testando aquiles

--- Aquiles/function_test/class_test.py
+++ Aquiles/function_test/class_test.py
@@ -1,13 +1,25 @@
 from GUI.sistema_estoque.filial import Filial
+from animations.loading import Animation
+from GUI.sistema_estoque.matriz import Matriz
+from GUI.sistema_estoque.produto import Produto
 
 class Func_T():
     
     def __init__(self):
         self
     
-    def test_f():
+    def test_f_f():
         f = Filial()
-        f.riscar()
+        f.create_filial('Area 51', 'Rua Perimental', 'Filial')
+        Animation.loading()
 
-t1 = Func_T()
-t1.test_f()
+    def test_f_m():
+        m = Matriz()
+        m.create_matriz('Area 67', 'Rua Nova Esperança', 'Matriz')
+        Animation.loading()
+
+    def test_f_p():
+        p = Produto()
+        p.create_product('Cuscuz', '1.00', '888', 'Area 52')
+        Animation.loading()
+    
